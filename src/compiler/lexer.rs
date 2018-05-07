5b67191b7a2c1dcea5718572a6086766dc1cd2ad
Nitzanel <nitzan@claysciences.com>
2018-05-07 23:45:26 +0300

    organized tests

--- src/compiler/lexer.rs
+++ src/compiler/lexer.rs
@@ -71,3 +71,43 @@ impl<'a> Lexer<'a> {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_identifier_token() {
+        let mut lex = Lexer::new(" def hello");
+        assert_eq!(lex.get_token(), Token::Def);
+        assert_eq!(lex.get_token(), Token::Identity("hello".to_string()));
+        assert_eq!(lex.get_token(), Token::EOF);
+    }
+
+    #[test]
+    fn test_number_token() {
+        let mut lex = Lexer::new("2 34. 51.23 .1");
+        assert_eq!(lex.get_token(), Token::Number(2.));
+        assert_eq!(lex.get_token(), Token::Number(34.));
+        assert_eq!(lex.get_token(), Token::Number(51.23));
+        assert_eq!(lex.get_token(), Token::Number(0.1));
+        assert_eq!(lex.get_token(), Token::EOF);
+    }
+
+    #[test]
+    fn test_unknown_token() {
+        let mut lex = Lexer::new("?");
+        assert_eq!(lex.get_token(), Token::Unknown('?'));
+    }
+
+    #[test]
+    fn test_comment_token() {
+        let mut lex = Lexer::new("#this is a comment\n 3.1");
+        assert_eq!(
+            lex.get_token(),
+            Token::Comment("this is a comment".to_string())
+        );
+        assert_eq!(lex.get_token(), Token::Number(3.1));
+        assert_eq!(lex.get_token(), Token::EOF);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,3 @@
-mod compiler;
-use compiler::{Lexer, Token};
-
 fn main() {
-    let mut lex = Lexer::new(" def hello");
-    assert_eq!(lex.get_token(), Token::Def);
-    assert_eq!(lex.get_token(), Token::Identity("hello".to_string()));
-    assert_eq!(lex.get_token(), Token::EOF);
-
-    lex = Lexer::new("2 34. 51.23 .1");
-    assert_eq!(lex.get_token(), Token::Number(2.));
-    assert_eq!(lex.get_token(), Token::Number(34.));
-    assert_eq!(lex.get_token(), Token::Number(51.23));
-    assert_eq!(lex.get_token(), Token::Number(0.1));
-    assert_eq!(lex.get_token(), Token::EOF);
-
-    lex = Lexer::new("?");
-    assert_eq!(lex.get_token(), Token::Unknown('?'));
-
-    lex = Lexer::new("#this is a comment\n 3.1");
-    assert_eq!(
-        lex.get_token(),
-        Token::Comment("this is a comment".to_string())
-    );
-    assert_eq!(lex.get_token(), Token::Number(3.1));
-    assert_eq!(lex.get_token(), Token::EOF);
+    println!("check the tests");
 }
